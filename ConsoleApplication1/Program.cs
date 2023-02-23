48338004ff9c03dadd80fa601875aa6f31e002c0
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-02-24 01:54:34 +0200

    Add convetror to PRN WITHOUT STACK AND LIST

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 class Program {
     static void Main(string[] args) {
@@ -7,44 +6,51 @@ class Program {
         Console.Write("Введіть вираз: ");
         string input = Console.ReadLine();
 
-        // Токенізація вхідного рядка
-        string[] tokens = new string[input.Length];
-        int tokenIndex = 0;
+        // Переведення в PRN
+        string[] output = new string[input.Length];
+        int outputIndex = 0;
+        string[] operators = new string[input.Length];
+        int operatorIndex = 0;
         string buffer = "";
+
+        // Цикл по всім символам рядка
         foreach (char c in input) {
             if (char.IsDigit(c)) {
+                // Якщо символ - цифра, додаємо його до буфера
                 buffer += c;
             }
             else if (c == ' ') {
+                // Якщо символ - пробіл, перевіряємо, чи був попередній символ число
                 if (buffer != "") {
-                    tokens[tokenIndex++] = buffer;
+                    output[outputIndex++] = buffer;
                     buffer = "";
                 }
             }
             else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')') {
+                // Якщо символ - оператор, перевіряємо, чи був попередній символ число
                 if (buffer != "") {
-                    tokens[tokenIndex++] = buffer;
+                    output[outputIndex++] = buffer;
                     buffer = "";
                 }
-                tokens[tokenIndex++] = c.ToString();
+                // Додаємо оператор до стеку
+                operators[operatorIndex++] = c.ToString();
             }
             else {
+                // Якщо символ невідомий, видаємо повідомлення про помилку
                 Console.WriteLine($"Невідомий символ: {c}");
                 return;
             }
         }
         if (buffer != "") {
-            tokens[tokenIndex++] = buffer;
+            output[outputIndex++] = buffer;
         }
 
-        // Виведення токенів на екран
-        Console.WriteLine("Токени:");
-        for (int i = 0; i < tokenIndex; i++) {
-            Console.WriteLine(tokens[i]);
+        // Витягуємо залишки операторів зі стеку та додаємо їх до вихідної черги
+        while (operatorIndex > 0) {
+            output[outputIndex++] = operators[--operatorIndex];
         }
+
+        // Виводимо результат
+        Console.WriteLine("PRN: " + string.Join(" ", output, 0, outputIndex));
     }
 }
-
-
-
-
