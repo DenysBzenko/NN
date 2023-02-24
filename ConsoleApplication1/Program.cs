0845ca923e0aef87a61ff93ff80c63356c62b4a8
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-02-24 02:44:27 +0200

    Add calculating

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
-class Program {
-    static void Main(string[] args) {
+class Program
+{
+    static void Main(string[] args)
+    {
         // Зчитати вхідний рядок з консолі
         Console.Write("Введіть вираз: ");
         string input = Console.ReadLine();
@@ -14,43 +16,97 @@ class Program {
         string buffer = "";
 
         // Цикл по всім символам рядка
-        foreach (char c in input) {
-            if (char.IsDigit(c)) {
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+            {
                 // Якщо символ - цифра, додаємо його до буфера
                 buffer += c;
             }
-            else if (c == ' ') {
+            else if (c == ' ')
+            {
                 // Якщо символ - пробіл, перевіряємо, чи був попередній символ число
-                if (buffer != "") {
+                if (buffer != "")
+                {
                     output[outputIndex++] = buffer;
                     buffer = "";
                 }
             }
-            else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')') {
+            else if (c == '+' || c == '-' || c == '*' || c == '/')
+            {
                 // Якщо символ - оператор, перевіряємо, чи був попередній символ число
-                if (buffer != "") {
+                if (buffer != "")
+                {
                     output[outputIndex++] = buffer;
                     buffer = "";
                 }
+
                 // Додаємо оператор до стеку
                 operators[operatorIndex++] = c.ToString();
             }
-            else {
+            else
+            {
                 // Якщо символ невідомий, видаємо повідомлення про помилку
                 Console.WriteLine($"Невідомий символ: {c}");
                 return;
             }
         }
-        if (buffer != "") {
+
+        if (buffer != "")
+        {
             output[outputIndex++] = buffer;
         }
 
         // Витягуємо залишки операторів зі стеку та додаємо їх до вихідної черги
-        while (operatorIndex > 0) {
+        while (operatorIndex > 0)
+        {
             output[outputIndex++] = operators[--operatorIndex];
         }
 
+        // Обчислення значення виразу з токенів в інверсній польській нотації
+        double result = 0;
+        double op1, op2;
+        string op;
+
+        for (int i = 0; i < outputIndex; i++)
+        {
+            if (double.TryParse(output[i], out op1))
+            {
+                // Якщо токен - число, додаємо його до стеку
+                operators[operatorIndex++] = output[i];
+            }
+            else
+            {
+                // Якщо токен - оператор, вилучаємо два операнди зі стеку та застосовуємо оператор до них
+                op = output[i];
+                op2 = double.Parse(operators[--operatorIndex]);
+                op1 = double.Parse(operators[--operatorIndex]);
+
+                switch (op)
+                {
+                    case "+":
+                        result = op1 + op2;
+                        break;
+                    case "-":
+                        result = op1 - op2;
+                        break;
+                    case "*":
+                        result = op1 * op2;
+                        break;
+                    case "/":
+                        result = op1 / op2;
+                        break;
+                    default:
+                        Console.WriteLine($"Невідомий оператор: {op}");
+                        return;
+                }
+
+                // Додаємо результат до стеку
+                operators[operatorIndex++] = result.ToString();
+            }
+        }
+
         // Виводимо результат
-        Console.WriteLine("PRN: " + string.Join(" ", output, 0, outputIndex));
+        Console.WriteLine($"Результат: {result}");
     }
 }
